c923f93f12e72e40c0a831cafdc769c18c6eb2b8
fauzanafism <fauzan137@gmail.com>
2021-11-24 06:42:46 +0700

    completed landing_page UI

---
+++ lib/ui/landing_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+            constraints: BoxConstraints.expand(),
+            child: Image(
+              image: AssetImage("assets/images/bg_start.jpg"),
+              fit: BoxFit.fitHeight,
+            )),
+        Container(
+          alignment: Alignment.topCenter,
+          margin: EdgeInsets.only(top: 170),
+          child: Material(
+            color: Colors.transparent,
+            child: Text(
+              "welcome to",
+              style: TextStyle(fontSize: 12, fontFamily: "Montserrat", color: Color(0xfff5a623)),
+            ),
+          ),
+        ),
+        Container(
+          child: Image(
+            image: AssetImage("assets/images/metrik_start.png"),
+          ),
+        ),
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 100),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Color(0xfff5a623),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30))),
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "START",
+                    style: TextStyle(
+                        fontFamily: "Montserrat",
+                        color: Colors.black,
+                        fontWeight: FontWeight.w900),
+                  ),
+                )),
+          ),
+        )
+      ],
+    );
+  }
+}
