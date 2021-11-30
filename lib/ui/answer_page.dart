2b82af87baa84c5b3c80ffdb4b0f7314acbac921
fauzanafism <fauzan137@gmail.com>
2021-12-01 06:56:14 +0700

    added basic UI

---
+++ lib/ui/answer_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:metrik2019_flutter/ui/home_page.dart';
+import 'package:metrik2019_flutter/widgets/style.dart';
+
+class AnswerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          children: [
+            Text(
+              "Your Answer",
+              style: whiteTextMont.copyWith(color: Colors.black, fontSize: 16),
+            ),
+            Spacer(),
+            Container(),
+            Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return HomePage();
+                }));
+              },
+              child: Text("SELESAI", style: whiteTextMont.copyWith(color: Colors.black),),
+              style: blackButton.copyWith(backgroundColor: MaterialStateProperty.all(Colors.white)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
