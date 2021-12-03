0cabd8f935588e6cc4048fab0008b66af0624f6e
fauzanafism <fauzan137@gmail.com>
2021-12-03 07:46:01 +0700

    added signIn function

---
+++ lib/repository/auth_services.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  static FirebaseAuth _auth = FirebaseAuth.instance;
+
+  static Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user!;
+      return user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+        return null;
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+        return null;
+      }
+    }
+  }
+
+  
+}
