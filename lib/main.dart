3fd82a574ce5887cf35412459346fc35633c71a9
fauzanafism <fauzan137@gmail.com>
2021-12-03 08:15:36 +0700

    added ensureInitialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:metrik2019_flutter/bloc/viewpass_bloc.dart';
 import 'ui/landing_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
