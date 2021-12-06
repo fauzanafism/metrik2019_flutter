aba0471ba82e221a9220f6e0c8dbffa956871174
fauzanafism <fauzan137@gmail.com>
2021-12-06 08:17:21 +0700

    centered UI

--- lib/ui/answer_page.dart
+++ lib/ui/answer_page.dart
@@ -17,60 +17,63 @@ class AnswerPage extends StatelessWidget {
       },
       child: Scaffold(
         backgroundColor: mainColor,
-        body: Container(
-          margin: EdgeInsets.all(40),
-          child: Column(
-            children: [
-              Text(
-                "Your Answer",
-                style:
-                    whiteTextMont.copyWith(color: Colors.black, fontSize: 16),
-              ),
-              Spacer(),
-              Container(
-                child: BlocBuilder<UserBloc, UserState>(
-                  builder: (context, user) {
-                    return Column(
-                      children: [
-                        StreamBuilder<DocumentSnapshot>(
-                            stream: userAnswer.doc(user.user!.uid).snapshots(),
-                            builder: (_, snapshot) {
-                              if (snapshot.hasData) {
-                                Map<String, dynamic> data = snapshot.data!
-                                    .data() as Map<String, dynamic>;
-                                return Column(
-                                  children: [
-                                    for (int i = 1; i < 15; 1)
-                                      NumAnswer(i, data[i])
-                                  ],
-                                );
-                              } else {
-                                return Text("Loading");
-                              }
-                            }),
-                      ],
-                    );
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your Answer",
+                  style:
+                      whiteTextMont.copyWith(color: Colors.black, fontSize: 16),
+                ),
+                Spacer(),
+                Container(
+                  child: BlocBuilder<UserBloc, UserState>(
+                    builder: (context, user) {
+                      return Column(
+                        children: [
+                          StreamBuilder<DocumentSnapshot>(
+                              stream: userAnswer.doc(user.user!.uid).snapshots(),
+                              builder: (_, snapshot) {
+                                if (snapshot.hasData) {
+                                  Map<String, dynamic> data = snapshot.data!
+                                      .data() as Map<String, dynamic>;
+                                  return Column(
+                                    children: [
+                                      
+                                        NumAnswer(1, data['1'])
+                                    ],
+                                  );
+                                } else {
+                                  return Text("Loading");
+                                }
+                              }),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.popUntil(context, (route) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Jawaban berhasil diinput")));
+                      return route.isFirst;
+                    });
                   },
+                  child: Text(
+                    "SELESAI",
+                    style: whiteTextMont.copyWith(color: Colors.black),
+                  ),
+                  style: blackButton.copyWith(
+                      backgroundColor: MaterialStateProperty.all(Colors.white)),
                 ),
-              ),
-              Spacer(),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.popUntil(context, (route) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Jawaban berhasil diinput")));
-                    return route.isFirst;
-                  });
-                },
-                child: Text(
-                  "SELESAI",
-                  style: whiteTextMont.copyWith(color: Colors.black),
-                ),
-                style: blackButton.copyWith(
-                    backgroundColor: MaterialStateProperty.all(Colors.white)),
-              ),
-              Spacer()
-            ],
+                Spacer()
+              ],
+            ),
           ),
         ),
       ),
